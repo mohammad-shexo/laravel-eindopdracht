dcdd4a62d4848b6997b5e99ed6bd8935044451cc
mohammad-shexo <mohammad.ibrahim.shexo@gmail.com>
2026-01-08 00:00:38 +0100

    Add admin role and authorization middleware

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/admin-test', function () {
+    return 'ADMIN OK';
+})->middleware(['auth', 'is_admin']);
+
+
 require __DIR__.'/auth.php';
